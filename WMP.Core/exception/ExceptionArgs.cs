fb9bd8f80de70b89ecbc851535726a3ad448acbf
oovkang <oov.kang@gmail.com>
2019-01-11 18:01:20 +0900

    custom error 추가

---
+++ WMP.Core/exception/ExceptionArgs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WMP.Core {
+    [Serializable]
+    public abstract class ExceptionArgs {
+        public virtual string Message { get { return string.Empty; } }
+    }
+}
